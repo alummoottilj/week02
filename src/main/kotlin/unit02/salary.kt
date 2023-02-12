39164eb542a9105a5aa38092628e4b450d58786f
Joshua <alummoottiljosh@gmail.com>
2023-02-12 07:10:56 -0600

    Made changes to files

---
+++ src/main/kotlin/unit02/salary.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var name: String = "Joshua"
+    var age: Int = 20
+    var annualPay: Double = 100000.0
+    println("My name is $name, my age is $age and i hope to earn $annualPay per year.")
+
+}
