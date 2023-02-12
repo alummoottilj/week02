50921e1fa89931e9179c9e9b31b04c1014994646
Joshua <alummoottiljosh@gmail.com>
2023-02-12 06:48:22 -0600

    Made changes to files

---
+++ src/main/kotlin/unit02/variables.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val myAge = 20
+    var averageAge: Double = 20.0
+    averageAge = (averageAge + 30) / 2
+
+    val testNumber: Int = 50
+    val evenOdd: Int = testNumber % 2
+
+
+    var answer = 0
+    answer += 1
+    answer += 10
+    answer *= 10
+    answer = answer shr 3
+    print(answer)
+}
