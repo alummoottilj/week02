f52bf3e110cc1a6d9f82916215418bcfb630d9fc
Joshua <alummoottiljosh@gmail.com>
2023-02-12 23:03:27 -0600

    Made changes to files

---
+++ src/main/kotlin/unit02/salesPrediction.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val totalSales = 4600000
+    val eastCoast = 0.62
+    val eastCoastSales = totalSales * eastCoast / 1000000.0
+
+    println("The East Coast will make" + eastCoastSales + "million dollars this year.")
+}
